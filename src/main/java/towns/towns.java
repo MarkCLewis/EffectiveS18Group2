a2ce2c0fb31e7f73a6f4f480c875064a22dcffd0
bwongcho <bwongcho@trinity.edu>
2018-02-20 13:40:19 -0600

    Update towns.java

--- src/main/java/towns/towns.java
+++ src/main/java/towns/towns.java
@@ -11,24 +11,28 @@ public class Towns {
 
 
 	
-	public Town(Pair<Double, Double> cent, Double rad) {
+	public static Town(Pair<Double, Double> cent, Double rad) {
 		radius= rad;
 		center= cent;
-		Arr= GenerateCenters(center, radius); 
+		Genera= GenerateCenters(center, radius); 
 
 	}
   
-        public GenerateCenters (Pair<Double, Double> cent, Double rad){
-		shop= {Pair(cent.left-sqrt(Math.pow(rad,2)/2), cent.right-sqrt(Math.pow(rad,2)/2))};
+        public static double [] GenerateCenters (Pair<Double, Double> cent, Double rad){
+		shop= {Pair(cent.left-sqrt(Math.pow(rad,2)/2), cent.right-sqrt(Math.pow(rad,2)/2))}; 
+		//shopCenter
 		house= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
+		//houseCenter
 		park={Pair(cent.left-sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
+		//parkCenter
 		school= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left-sqrt(rad^2)/2))};
+		//schoolCenter
 	}
-	//public Shops(cent,rad)
-	//public House(cent,rad)
-	//public Park(cent,rad)
-	//public School(cent,rad)
-	//sin(theta)= 
+	public createShops()
+	public House(cent,rad)
+	public Park(cent,rad)
+	public School(cent,rad)
+	 
 
 
 	
