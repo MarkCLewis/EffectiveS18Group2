c3121f8b515c6059691fe3c8c2c6876e6c874e5c
bwongcho <bwongcho@trinity.edu>
2018-02-20 13:56:47 -0600

    Update towns.java

--- src/main/java/towns/towns.java
+++ src/main/java/towns/towns.java
@@ -8,33 +8,39 @@ import virtualworld.terrain.Pair;
 
 
 public class Towns {
+        private douhle radius, center;
+	private double [] middle;
+	private Pair Shopcenter, Schoolcenter, Housecenter, Parkcenter;
 
 
 	
 	public static Town(Pair<Double, Double> cent, Double rad) {
 		radius= rad;
 		center= cent;
-		Genera= GenerateCenters(center, radius); 
-
+		middle= GenerateCenters(center, radius); 
+		Shopscenter= middle[0];
+		Housecenter= middle [1];
+		Parkcenter= middle [2];
+		Schoolcenter= middle [3];
 	}
   
         public static double [] GenerateCenters (Pair<Double, Double> cent, Double rad){
-		shop= {Pair(cent.left-sqrt(Math.pow(rad,2)/2), cent.right-sqrt(Math.pow(rad,2)/2))}; 
+		Pair shop= {Pair(cent.left-sqrt(Math.pow(rad,2)/2), cent.right-sqrt(Math.pow(rad,2)/2))}; 
 		//shopCenter
-		house= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
+		Pair house= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
 		//houseCenter
-		park={Pair(cent.left-sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
+		Pair park={Pair(cent.left-sqrt(Math.pow(rad,2)/2),cent.left+sqrt(rad^2)/2))};
 		//parkCenter
-		school= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left-sqrt(rad^2)/2))};
+		Pair school= {Pair(cent.left+sqrt(Math.pow(rad,2)/2),cent.left-sqrt(rad^2)/2))};
 		//schoolCenter
+		double[] passArray = new double[]{1,2,3,4,5,6,7,8,9,10}; 
+		return (pass);
 	}
-	public createShops()
-	public House(cent,rad)
-	public Park(cent,rad)
-	public School(cent,rad)
+	//public createShops(cent, rad)
+	//public House(cent,rad)
+	//public Park(cent,rad)
+	//public School(cent,rad)
 	 
 
-
-	
 
 }
