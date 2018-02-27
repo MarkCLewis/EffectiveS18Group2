2cc385492b42343e27103281ad05a3834d97cba2
Ben Spears <bspears@trinity.edu>
2018-02-26 22:22:52 -0600

    wrote complete functions for Node class in WorldManager

--- src/main/java/worldmanager/Node.java
+++ src/main/java/worldmanager/Node.java
@@ -7,19 +7,39 @@ public class Node {
 	//terrain
 	Terrain terrain;
 
-	//Nodes for traceback
-	Node parent;
-	Node[] children;
+	//Nodes for traversal
+	Node parent = null;
+	Node[] children = null;
+	
+	//Constructor
+	public Node(Terrain t) {
+		terrain = t;
+	}
+	
+	//updating when connecting to parent or child
+	public void updateParent(Node p) {
+		parent = p;
+	}
+	
+	public void updateChild(Node[] c) {
+		children = c;
+	}
 	
 	//Functions to implement
-	private Pair center() {
-		//TODO
+	private Pair<Double,Double> center() {
+		return terrain.getCenter();
 	}
 	
 	public Node findCamera() {
-		//TODO
+		Node current = this;
+		while(current.parent != null) {
+			current = current.parent;
+		}
+		return current;
 	}
 	
+	// Function to split. Recommended by Ian.
+	// Will talk later to see what the functionality for it he had in mind was as Terrain already has a similar function.
 	private void split() {
 		//TODO
 	}
