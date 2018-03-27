332f403d11e12443f11ff5170739dd21657d7034
Ian Witecki <ianwitecki@gmail.com>
2018-03-27 12:42:40 -0500

    Added splitting for terrain and updated height rendering

--- src/main/java/virtualworld/terrain/Terrain.java
+++ src/main/java/virtualworld/terrain/Terrain.java
@@ -3,8 +3,15 @@ package virtualworld.terrain;
 public class Terrain {
 	
 	public static void main(String[] args) {
-	    
-		Terrain t = new Terrain(new Point(0.0,0.0), 100, 10);
+	   
+		double length = 100;
+		int seed = 10;
+		double[][] heightMap = {{0.0, 0.0 , 0.0, 0.0},
+								{0.0, 0.0 , 0.0, 0.0},
+								{0.0, 0.0 , 0.0, 0.0},
+								{0.0, 0.0 , 0.0, 0.0}};
+	
+		Terrain t = new Terrain(new Point(0.0,0.0), length, seed, heightMap);
 		
 		double[][] render = t.renderHeights();
 		
@@ -19,18 +26,23 @@ public class Terrain {
 	
     // (x, z) coordinate of the terrain center
 	//private final Pair<Double, Double> center;
+	private final double[][] heightMap;
 	private final Point center;
 	private final Perlin perlNoise = Perlin.getInstance();
+	private boolean mapIsSet;
 
-    // length and points per side of the area that the terrain covers.
+    // length the area that the terrain covers and the height seed between each point.
     private final double length;
-    private final int sides;
+    private final double heightSeed;
+    
     
 	
-	public Terrain(Point c, double len, int side) {
+	public Terrain(Point c, double len, double heightS, double[][] constructionArray) {
 		center = c;
         length = len;
-        sides = side;
+        heightMap = expandArray(constructionArray);
+        heightSeed = heightS;
+        mapIsSet = false;
 	}
 
     //returns a pair that represents the center of the terrain object
@@ -40,88 +52,107 @@ public class Terrain {
 
     // splits the terrain object into four separate terrain objects
     public Terrain[] split() {
+    	
+        double[][][] subTerrains = splitArray();
         return new Terrain[] {
+        	
             new Terrain(
                 new Point(
                 		center.getX() - (length/4), 
                 		center.getY() - (length/4)),
                 length,
-                sides),
-            new Terrain(
-                new Point(
-                		center.getX() + (length/4), 
-                		center.getY() - (length/4)),
-                length,
-                sides),
+                heightSeed,
+                subTerrains[0]),
             new Terrain(
                 new Point(
                 		center.getX() - (length/4), 
-                		center.getY() + (length/4)),
+                		center.getY() - (length/4)),
                 length,
-                sides),
+                heightSeed,
+                subTerrains[1]),
             new Terrain(
                 new Point(
-                		center.getX() + (length/4), 
-                		center.getY() + (length/4)),
+                		center.getX() - (length/4), 
+                		center.getY() - (length/4)),
                 length,
-                sides)
+                heightSeed,
+                subTerrains[2]),
+            new Terrain(
+                new Point(
+                		center.getX() - (length/4), 
+                		center.getY() - (length/4)),
+                length,
+                heightSeed,
+                subTerrains[3]),
         };   
+    }
+    
+    private double[][] expandArray(double[][] init) {
+    	
+    	double[][] ans = new double [(init.length*init.length) - 1][(init.length*init.length) - 1];
+    	
+    	for (int r = 0; r < init.length; r++) {
+			for (int c = 0; c < init.length; c++) {
+				ans[r*2][c*2] = init[r][c];
+			}
+    	}
+    	
+    	return ans;
+    }
+    
+    //function takes in a 0,1,2,3 for the 4 different parts of the terrain
+    private double[][][] splitArray() {
+    	
+    	if(!mapIsSet) renderHeights();
+    	
+    	double[][][] split = new double[4][heightMap.length/2][heightMap.length/2];
+
+    	//for(int f = 0; f < split.length; f++) {
+    	//}
+    	
+    	return split;
+    	
     }
     
     //generates a height map for terrain square
     public double[][] renderHeights() {
+    	// height difference seed
     	
-    	double[][] generatedY = new double[sides][sides];
-    	
-    	generatedY[0][0] = 0.25;
-    	
-    	//int j = 0;
-    	//int k = 0;
-    	
-    	double yOffset = (center.getX() - length/2) / 10;
-    	for (int r = 0; r < sides; r++) {
-    		double xOffset = (center.getY() + length/2) / 10;
-    		for (int c = 0; c < sides; c++) {
-    			if (c == 0) {
-    				if (r != 0) {
-    					generatedY[r][c] = perlNoise.noise2D(xOffset, generatedY[r-1][c])  *10;
-    					xOffset += 0.1;
-    				}
-    			} else if (r == 0){
-    				if (c != 0) {
-    					generatedY[r][c] = perlNoise.noise2D(xOffset, generatedY[r][c-1])  *10;
-    					xOffset += 0.1;
-    				}
-    			}
-    			else if (c != 0 && r != 0) {
-    				generatedY[r][c] = ((perlNoise.noise2D(xOffset, generatedY[r][c-1]) + perlNoise.noise2D(xOffset, generatedY[r-1][c])) / 2) *10;
-    				xOffset += 0.1;
-    				System.out.println(generatedY[r][c]);
-    			}
-    		}
-    		yOffset += 0.1;
+    	for (int r = 0; r < heightMap.length; r+= 2) {
+    		double xOffset = (center.getY() + length/2) / heightSeed;
+			for (int c = 0; c < heightMap.length; c+= 2) {
+				//average of all of the perlin generated heights from surrounding heights
+				heightMap[r][c] = ((perlNoise.noise2D(xOffset, heightMap[r-1][c-1])*heightSeed) +
+									(perlNoise.noise2D(xOffset, heightMap[r+1][c-1])*heightSeed) +
+									(perlNoise.noise2D(xOffset, heightMap[r-1][c+1])*heightSeed) +
+									(perlNoise.noise2D(xOffset, heightMap[r+1][c+1])*heightSeed)) / 2;
+    			xOffset += 0.1;
+			}
     	}
     	
-    	// Perlin Noise 3D Failed Code
-    	/*for ( double r = (center.getLeft() - length/2); r <= (center.getLeft() + length/2); r += (length/sides)) {
-    		for ( double c = (center.getRight() + length/2); c <= (center.getRight() - length/2); c -= (length/sides)) {
-        		if (r == center.getLeft() - length/2) {
-        			if (k != 0) {
-        				//noise generated by preceding point in column
-        				generatedY[j][k] = perlNoise.noise(r, generatedY[j][k-1], c);
-        			}
-        		} else if (c == center.getLeft() + length/2){
-        			generatedY[j][k] = perlNoise.noise(r, generatedY[j-1][k], c);
-        		} else {
-        			//average of two closest points generated noise
-        			generatedY[j][k] = perlNoise.noise(r, generatedY[j][k-1], c) + perlNoise.noise(r, generatedY[j-1][k], c) / 2;
-        		}
-    			k++;
-        	}
-    		j++;
-    	}*/
     	
-    	return generatedY;
+    	for (int r = 0; r < heightMap.length; r++) {
+    		double xOffset = (center.getY() + length/2) / 10;
+			for (int c = 0; c < heightMap.length; c++) {
+				if (heightMap[r][c] != 0) {
+					if (r % 2 == 0) {
+						//vertical triangle case
+						heightMap[r][c] = ((perlNoise.noise2D(xOffset, heightMap[r][c-1])*heightSeed) +
+											(perlNoise.noise2D(xOffset, heightMap[r][c+1])*heightSeed) +
+											(perlNoise.noise2D(xOffset, heightMap[r+1][c])*heightSeed)) / 2;
+					} else {
+						//horizontal triangle case
+						heightMap[r][c] = ((perlNoise.noise2D(xOffset, heightMap[r+1][c])*heightSeed) +
+											(perlNoise.noise2D(xOffset, heightMap[r-1][c])*heightSeed) +
+											(perlNoise.noise2D(xOffset, heightMap[r][c+1])*heightSeed)) / 2;
+						
+					}
+					xOffset += 0.1;
+			}
+		}
+    	}
+    	
+    	return heightMap;
     	
     }
 
