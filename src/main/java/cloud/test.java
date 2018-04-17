3f01c929c17427650f76445ac87b168747b7fffd
Thoai Bui <tbui1@titan11.cs.trinity.edu>
2018-04-17 13:58:40 -0500

    add some functions

--- src/main/java/cloud/test.java
+++ src/main/java/cloud/test.java
@@ -15,15 +15,15 @@ public class test {
 		//Sphere s = new Sphere((float)0.5, 0 ,3, 3);
 		
 		//distinct pattern
-		//Cloud t = new Cloud (5,375,0,50,5,50);
+		//Cloud t = new Cloud (5,375,0,100,20,100);
 		//t.setOffSets(.75, 1, 3, 2, 8);		
 		//t.setOffSets(3,5,2,2,4);
 		
 		//cluster
 		Cloud t = new Cloud (5,400,0,50,15,35);
-		//Cloud t = new Cloud (5,400,0,30,10,20);
-		t.setOffSets(6.5, 3, 3, 2, 8);
-		//t.setOffSets(2, 1, 1, 2, 4); //blocky cloud
+		//Cloud t = new Cloud (5,500,0,30,10,20);
+		//t.setOffSets(6.5, 3, 3, 2, 8);
+		t.setOffSets(2, 1, 1, 2, 4); //blocky cloud
 		t.getInverse();
 		//Cloud t = new Cloud (5,300,0,100,30,100);
 		ArrayList<Shape> spheres = t.makeShape3d();
@@ -36,12 +36,6 @@ public class test {
 
 			g.addShapes(spheres);
 			//g.addShapes(s2);
-		
-/*		for (Shape sp : s2)
-		{
-			g.addShape(sp);
-		}
-*/
 		g.start();
 		System.out.println(spheres.size());
 
