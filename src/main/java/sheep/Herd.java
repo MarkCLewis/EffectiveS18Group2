b2cc0cbc56eefcc0b7f1f3cd5c0eb968192386a3
Cristina <ctrevin1@trinity.edu>
2018-05-05 07:08:06 -0500

    Sheeping around

--- src/main/java/sheep/Herd.java
+++ src/main/java/sheep/Herd.java
@@ -1,24 +1,71 @@
 package sheep;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import shapes.Cylinder;
+import shapes.Shape;
+import shapes.Sphere;
+import virtualworld.terrain.Point;
+
 public class Herd {
-	/*private Herd(int cen, int rad) {
-		center = cen;
-		radius = rad;
-		herd = new double[cen][rad];
+	private Herd(Point loc) {
+		location = loc;
+		List<Sheep> herd = new ArrayList<Sheep>();
+	}
+
+	Point location;
+	public List<Sheep> herd;
+	private double xPos = location.getX();
+	private double zPos = location.getZ();
+	private double yPos = 0;
+
+	// sphere arguments: radius, xPos, yPos, zPos
+	List<Shape> Sheep() {
+		List<Shape> sheep = new ArrayList<Shape>();
+		Sphere body = new Sphere(20, xPos, yPos, zPos);
+		Sphere head = new Sphere(10, xPos-20, yPos, zPos-20);
+		Cylinder fl = new Cylinder(20, 2, xPos-10, yPos-10, zPos+10);
+		Cylinder fr = new Cylinder(20, 2, xPos-10, yPos-10, zPos-10);
+		Cylinder bl = new Cylinder(20, 2, xPos+10, yPos-10, zPos+10);
+		Cylinder br = new Cylinder(20, 2, xPos+10, yPos-10, zPos+10);
+		
+		sheep.add(body);
+		sheep.add(head);
+		sheep.add(fl);
+		sheep.add(fr);
+		sheep.add(bl);
+		sheep.add(br);
+		return sheep;
 	}
 	
-	private final int center;
-	private final int radius;
-	public double [][] herd;
-*/
-/*	public Sheep(int width, int length, int height) {
-			int w = width;
-			int l = length;
-			int h = height;
-			// TODO makeSheep in here
-		}*/
-
-	public void drawSheep() {
-
+	double getXBound(Sheep sheep){
+		return 0;
 	}
+
+	List<Shape> drawSheep(int xPos, int yPos, int zPos) {
+		List<Shape> sheep = new ArrayList<Shape>();
+		Sphere body = new Sphere(20, xPos, yPos, zPos);
+		Sphere head = new Sphere(10, xPos-20, yPos, zPos-20);
+		Cylinder fl = new Cylinder(20, 2, xPos-10, yPos-10, zPos+10);
+		Cylinder fr = new Cylinder(20, 2, xPos-10, yPos-10, zPos-10);
+		Cylinder bl = new Cylinder(20, 2, xPos+10, yPos-10, zPos+10);
+		Cylinder br = new Cylinder(20, 2, xPos+10, yPos-10, zPos+10);
+		
+		sheep.add(body);
+		sheep.add(head);
+		sheep.add(fl);
+		sheep.add(fr);
+		sheep.add(bl);
+		sheep.add(br);
+		return sheep;
+	}
+	
+	List<Sheep> makeHerd(Point location){
+		List<Sheep> herd = new ArrayList<Sheep>();
+		
+		return herd;
+	}
+	
+	//boolean collision(Point loc, )
 }
--- src/main/java/sheep/TestPrint.java
+++ src/main/java/sheep/TestPrint.java
@@ -1,36 +1,52 @@
 package sheep;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import engine.Game;
-import shapes.Shape;
+import shapes.Cylinder;
 import shapes.Sphere;
 
 public class TestPrint {
-	
+
 	public static void main(String[] args) {
 		Game g = new Game(400f);
-		g.start();	
-		
+		g.start();
+
+		/*
+		 * public static void main(String[] args) { Game g = new Game(250f);
+		 * Point center = new Point(0,0);
+		 * 
+		 * WorldManager world = WorldManager.getInstance();
+		 * 
+		 * //Sphere s = new Sphere(0 ,3, 3);
+		 * 
+		 * Terrain t = Terrain.forFeilds(center, 20000, 16000); Terrain[] ters =
+		 * t.split(); List<shapes.Shape> shapes = new ArrayList<shapes.Shape>();
+		 * for(int i = 0; i < ters.length; i++) { Terrain[] tmpters =
+		 * ters[i].split(); shapes.add(tmpters[0].getHeightMapSurface());
+		 * shapes.add(tmpters[1].getHeightMapSurface());
+		 * shapes.add(tmpters[2].getHeightMapSurface());
+		 * shapes.add(tmpters[3].getHeightMapSurface()); } g.start();
+		 * g.addShapes(shapes); //g.addShape(s); }
+		 */
+
 		// Practice implementation
-		//sphere arguments: radius, xPos, yPos, zPos
-		//cylinder arguments: height, radius, xPos, yPos, zPos
-		Sphere body = new Sphere(20, 20, 400, 20);
+		// sphere arguments: radius, xPos, yPos, zPos
+		// cylinder arguments: height, radius, xPos, yPos, zPos
+		Sphere body = new Sphere(20, 20, 400, 0);
 		Sphere head = new Sphere(10, 0, 400, 0);
-		//Cylinder legFL = new Cylinder(20, 2, 30, 380, 20);
-		//Cylinder legFR = new Cylinder(20, 2, 30, 380, 30);
-		//Cylinder legBL = new Cylinder(20, 2, 30, 380, 40); //ok
-		//Cylinder legBR = new Cylinder(20, 2, 40, 380, 50); //xpos ox
+		Cylinder legFL = new Cylinder(20, 2, 10, 390, 10);
+		Cylinder legFR = new Cylinder(20, 2, 10, 390, -10);
+		Cylinder legBL = new Cylinder(20, 2, 30, 390, 10); //ok
+		Cylinder legBR = new Cylinder(20, 2, 30, 390, -10); //xpos ox
 		g.addShape(body);
 		g.addShape(head);
-		//g.addShape(legFL);
-		//g.addShape(legFR);
-		//g.addShape(legBL);
-		//g.addShape(legBR);
+		g.addShape(legFL);
+		g.addShape(legFR);
+		g.addShape(legBL);
+		g.addShape(legBR);
+
+		// SheepBuilder build = SheepBuilder.getInstance();
+		// List<Shape> shapes = new ArrayList<Shape>();
 		
-		//SheepBuilder build = SheepBuilder.getInstance();
-		//List<Shape> shapes = new ArrayList<Shape>();
 
 	}
 }
