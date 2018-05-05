4eedd0c9c4cbb953ae606974c37b38a87f593cb0
Thoai Bui <tbui1@trinity.edu>
2018-05-05 01:53:28 -0500

    add testdist file

--- src/main/java/cloud/test.java
+++ src/main/java/cloud/test.java
@@ -14,23 +14,14 @@ public class test {
 	public static void main(String[] args) {
 		Game g = new Game(100f);
 		CloudFactory cFactory = CloudFactory.getInstance();
-
-		
-		
+	
 		//testing geneting on terrain size
-
-		
 		Terrain t = Terrain.forMountains(new Point(0000,0000), 2048, 6);
-
-		
-		
 		
 		List<Shape> shapes = new ArrayList<Shape>();
 		//shapes.addAll(t.getShapes());
-		
-		
 		//Method to create a list of cloud on a given terrain 
-		List<Cloud> clouds = cFactory.getClouds( new Point(1000,1000), 16000);
+		List<Cloud> clouds = cFactory.getClouds( new Point(0,0), 16000);
 		//shapes.add(new RectangularPrism(3000, 10, 1000, 0, 1000, 0));
 		
 		for (Cloud cloud : clouds)
@@ -38,31 +29,30 @@ public class test {
 			cloud.distFromCamera(1000);
 			shapes.addAll(cloud.getShapes());
 		}
-		shapes.addAll(t.getShapes());
+		//shapes.addAll(t.getShapes());
 	
 		Cloud cloud = cFactory.getCloud(0, 0, 0, 0, cFactory.getRandomSize(0));
-		Cloud cloud2 = cFactory.getCloud(0, 0, 0, 1, cFactory.getRandomSize(1));
-		Cloud cloud3 = cFactory.getCloud(0, 0, 0, 2, cFactory.getRandomSize(2));
-		Cloud cloud4 = cFactory.getCloud(0, 0, 0, 3, cFactory.getRandomSize(3));
-		Cloud cloud5 = cFactory.getCloud(0, 0, 0, 4, cFactory.getRandomSize(4));
+		Cloud cloud2 = cFactory.getCloud(100, 0, 100, 1, cFactory.getRandomSize(1));
+		Cloud cloud3 = cFactory.getCloud(200, 0, 200, 2, cFactory.getRandomSize(2));
 		
+		List<Shape> shape2 = new ArrayList<Shape>();
+		shape2.addAll(cloud.getShapes());
+		shape2.addAll(cloud2.getShapes());
+		shape2.addAll(cloud3.getShapes());
+		
+		Cloud cloud4 = cFactory.getCloud(1000, 0, 0, 3, cFactory.getRandomSize(3));
+		Cloud cloud5 = cFactory.getCloud(0, 3000, 0, 4, cFactory.getRandomSize(4));
+		
+		List<Shape> shape3 = new ArrayList<Shape>();
+		//shape3.addAll(cloud4.getShapes());
+		
+		shape3.addAll(cloud5.getShapes());
 		g.start();
 		
-		//g.addShapes(cloud.getShapes());
-		//g.addShapes(cloud2.getShapes());
-		//g.addShapes(cloud3.getShapes());
-		//g.addShapes(cloud4.getShapes());
-		//g.addShapes(cloud5.getShapes());
-		
-		
-		g.addShapes(shapes);
+		g.addShapes(shape2);		
+		//g.addShapes(shape3);
+		//g.addShapes(shapes);
 
-
-	
-		
-		
-		
-		//System.out.println(spheres.size());
 	}
 
 }
--- src/main/java/cloud/testDist.java
+++ src/main/java/cloud/testDist.java
@@ -1,19 +1,90 @@
 package cloud;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import engine.Game;
+import shapes.Shape;
 
 public class testDist {
+	static CloudFactory cFactory = CloudFactory.getInstance();
 	public static void main(String[] args) {
 		Game g = new Game(100f);
-		CloudFactory cFactory = CloudFactory.getInstance();
 		
-		int type = 0;
-		double size = cFactory.getRandomSize(0);
-		
-		Cloud a = cFactory.getCloud(0, 0, 0, type, size);
+		List<Shape> shapes1 = typeOne();
+		List<Shape> shapes2 = typeTwo();
+		List<Shape> shapes3 = typeThree();
 		
 		g.start();
-		g.addShapes(a.getShapes());
+		g.addShapes(shapes1);
+		g.addShapes(shapes2);
+		g.addShapes(shapes3);
 		
+	}
+	
+	private static List<Shape> typeOne()
+	{
+		int type = 0;
+		double size = cFactory.getRandomSize(type);
+		List<Shape> shapes = new ArrayList<Shape>();	
+		
+		Cloud a = cFactory.getCloud(0, 0, 0, type, size);
+		a.distFromCamera(1000);
+		
+		Cloud b = cFactory.getCloud(200, 0, 0, type, size);
+		b.distFromCamera(2000);
+		
+		Cloud c = cFactory.getCloud(400, 0, 0, type, size);
+		c.distFromCamera(3000);
+		
+		
+		shapes.addAll(a.getShapes());
+		shapes.addAll(b.getShapes());
+		shapes.addAll(c.getShapes());
+		return shapes;
+	}
+	
+	private static List<Shape> typeTwo()
+	{
+		int type = 1;
+		double size = cFactory.getRandomSize(type);
+		List<Shape> shapes = new ArrayList<Shape>();	
+		
+		Cloud a = cFactory.getCloud(1000, 2000, 500, type, size);
+		a.distFromCamera(1000);
+		
+		Cloud b = cFactory.getCloud(1400, 2000, 500, type, size);
+		b.distFromCamera(2000);
+		
+		Cloud c = cFactory.getCloud(1800, 2000, 500, type, size);
+		c.distFromCamera(3000);
+		
+		
+		shapes.addAll(a.getShapes());
+		shapes.addAll(b.getShapes());
+		shapes.addAll(c.getShapes());
+		return shapes;
+	}
+	
+	private static List<Shape> typeThree()
+	{
+		int type = 2;
+		double size = cFactory.getRandomSize(type);
+		List<Shape> shapes = new ArrayList<Shape>();	
+		
+		Cloud a = cFactory.getCloud(2000, 3000, 1000, type, size);
+		a.distFromCamera(1000);
+		
+		Cloud b = cFactory.getCloud(2400, 3000, 1000, type, size);
+		b.distFromCamera(2000);
+		
+		Cloud c = cFactory.getCloud(2800, 3000, 1000, type, size);
+		c.distFromCamera(3000);
+		
+		
+		shapes.addAll(a.getShapes());
+		shapes.addAll(b.getShapes());
+		shapes.addAll(c.getShapes());
+		return shapes;
 	}
 }
